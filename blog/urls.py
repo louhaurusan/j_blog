53f868cf649ab3dd7f02fcec0daea3fd927e6efb
Jeannou <maud.lelu@gmail.com>
2015-07-04 23:10:25 +0200

    added views and templates

--- blog/urls.py
+++ blog/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 
 urlpatterns = [
-    url(r'^$', views.post_list),
+    url(r'^$', views.PostsView.as_view(), name="post_list"),
+    url(r'^post/(?P<pk>[0-9]+)/$', views.DetailView.as_view(), name="post_detail")
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,8 +1,23 @@
 from django.shortcuts import render
 from django.utils import timezone
+from django.views import generic
+
 from .models import Post
 
 
-def post_list(request):
-    posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('-published_date')
-    return render(request, 'blog/post_list.html', {'posts': posts})
+class PostsView(generic.ListView):
+    template_name = 'blog/post_list.html'
+    context_object_name = 'latest_post_list'
+
+    def get_queryset(self):
+        return Post.objects.order_by('-published_date')[:10]
+
+
+class DetailView(generic.DetailView):
+    model = Post
+    template_name = 'blog/post_detail.html'
+
+   # def post_list(request):
+   #   posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('-published_date')
+   #   return render(request, 'blog/post_list.html', {'posts': posts})
+
